2b7604ea3d73095c99941e724f7cada09feab92d
avsolatorio <avsolatorio@gmail.com>
2021-12-20 02:02:41 +0000

    Add dependencies and update numpy version.

--- setup.py
+++ setup.py
@@ -29,10 +29,13 @@ CLASSIFIERS = [
 INSTALL_REQUIRES = [
     "unidecode",
     "inflect==5.3.0",
+    "fasttext",
+    "polyglot",
+    "PyICU",
     "flashtext>=2.7,<3.0.0",
     "openpyxl==3.0.7",
     "spacy>=2.3.5,<=2.3.7",
-    "numpy>=1.19.1,<=1.19.4",
+    "numpy>=1.21.0,<=1.21.5",
     "spacy-langdetect==0.1.2",
     "wordninja==2.0.0",
     "pandas>=1.1.2,<=1.2.0",
@@ -46,7 +49,8 @@ INSTALL_REQUIRES = [
     "requests>2.24.0,<=2.25.1",
     "tika==1.24",
     "googletrans==3.1.0a0"]
-
+DEPENDENCY_LINKS=[
+    "git+https://github.com/avsolatorio/pycld2.git@d63195032c3a2c126cc4cbe1cb1c6de8cf8c8d2a"]
 PACKAGE_DIR = {'': 'src'}
 
 # Setting up
@@ -60,6 +64,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type=LONG_DESC_TYPE,
     install_requires=INSTALL_REQUIRES,
+    dependency_link=DEPENDENCY_LINKS,
     classifiers=CLASSIFIERS,
     package_dir=PACKAGE_DIR,
     packages=find_packages(include=['wb_cleaning'])
